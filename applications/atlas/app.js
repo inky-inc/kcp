4fc785b3cd1c97dcdb17abeea782878836916d9c
Forrest Akin <forrest.akin@gmail.com>
2016-07-02 12:56:39 -0700

    [refactor] refactors styles

--- applications/atlas/app.js
+++ applications/atlas/app.js
@@ -3,11 +3,11 @@
 const path = require('path');
 
 // Enable ES6 and Babel.
-require('babel-register')({})
+require('babel-register')({});
 // require('babel-polyfill')
 
 // Launch the Server.
 process.env.ATLAS = true;
 process.env.NODE_ENV = 'production';
-exports = require(path.join(__dirname, './server.jsx')).default
+exports = require(path.join(__dirname, './server.jsx')).default;
 
